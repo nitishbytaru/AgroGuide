03aa07b2c16d46f75d512350ee0d1371148fd0e8
nitishbytaru <bndnitish24@gmail.com>
2025-04-03 00:28:47 +0530

    add

--- server/app.py
+++ server/app.py
@@ -1,5 +1,6 @@
 import os
-from flask import Flask,jsonify
+import psutil
+from flask import Flask, jsonify
 from flask_cors import CORS
 from src.Routes.disease_routes import disease_routes
 from src.Routes.crop_suggestion import crop_prediction_routes
@@ -9,6 +10,7 @@ from dotenv import load_dotenv
 
 # Load environment variables from .env file
 load_dotenv()
+
 
 # Get frontend URL from environment variables (fallback to localhost if not set)
 frontend_url = os.environ.get("frontend_url")
@@ -28,9 +30,21 @@ CORS(
     supports_credentials=True,
 )
 
+
+@app.route("/memory")
+def memory_usage():
+    process = psutil.Process(os.getpid())  # Get current process
+    mem_info = process.memory_info()  # Get memory usage
+    return {
+        "rss_memory_MB": mem_info.rss / 1024 / 1024,  # Resident memory
+        "vms_memory_MB": mem_info.vms / 1024 / 1024,  # Virtual memory
+    }
+
+
 @app.route("/test")
 def test():
     return jsonify("it is working")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
